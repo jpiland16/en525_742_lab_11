5ff4117cb59662c2d6b0c20ab08af7da1e0f6cf8
Jonathan Piland <jonathan.piland@duke.edu>
2026-04-13 22:20:16 -0400

    milestone 2 - fiforeader

--- fiforeader/fiforeader.c
+++ fiforeader/fiforeader.c
@@ -2,8 +2,22 @@
 #include <sys/mman.h>
 #include <fcntl.h>
 #include "xllfifo.h"
+#include <stdlib.h>
 
-int main(void) {
+
+#define USAGE "\
+usage: ./fiforeader <number_of_words> \n\
+  - Reads <number_of_words> 4-byte words from the AXI-Stream FIFO in the PL\
+"
+
+int main(int argc, char* argv[]) {
+
+    if (argc != 2) {
+        puts(USAGE);
+        return EXIT_FAILURE;
+    }
+
+    int num_words_to_read = atoi(argv[1]);
 
     int fd = open("/dev/mem", O_RDWR | O_SYNC);
     if (fd < 0) {
@@ -12,11 +26,11 @@ int main(void) {
     }
 
     void *map = mmap(NULL,
-                     0x10000,
+                     XPAR_AXI_FIFO_MM_S_0_HIGHADDR - XPAR_AXI_FIFO_MM_S_0_BASEADDR + 1,
                      PROT_READ | PROT_WRITE,
                      MAP_SHARED,
                      fd,
-                     0x43c10000);
+                     XPAR_AXI_FIFO_MM_S_0_BASEADDR);
 
     if (map == MAP_FAILED) {
         perror("mmap");
@@ -25,12 +39,35 @@ int main(void) {
 
     volatile uint32_t *fifo_ptr = (volatile uint32_t *)map;
 
-    puts("Hello world!");
+    puts("Initializing FIFO...");
     XLlFifo fifo;
     XLlFifo_Initialize(&fifo, (unsigned int) fifo_ptr);
-    puts("Passed initialization!");
+    printf("Initialization complete! Reading %d 4-byte words...\n", num_words_to_read);
+    fflush(stdout); // To ensure that timing can be reviewed accurately
 
-    // int occ = XLlFifo_RxOccupancy(&fifo);
-    // printf("RX Occupancy: %d\n", occ);
+
+    // for (int i = 0; i < 17; i++) {
+    //     printf(" - fifo[BASE + 0x%02x] = 0x%08x\n", i * 4, fifo_ptr[i]);
+    // }
+
+    // for (int i = 0; i < 10; i++) {
+    //     int occ = XLlFifo_iRxOccupancy(&fifo);
+    //     printf("RX Occupancy: %d\n", occ);
+    // }
+
+    // unsigned int len = (XLlFifo_iRxGetLen(&fifo) & 0x3FFFF) >> 2;
+
+    unsigned int num_words_read = 0;
+    unsigned int single_sample;
+    
+    while (num_words_read < num_words_to_read) {
+        if (((XLlFifo_iRxGetLen(&fifo) & 0x3FFFF) >> 2) > 0) {
+            // There are bytes available in the FIFO, read them now
+            XLlFifo_Read(&fifo, &single_sample, 4);
+            num_words_read++;
+        }
+    }
+
+    puts("Goodbye");
 
 }
--- test_fiforeader.py
+++ test_fiforeader.py
@@ -1,13 +1,17 @@
 from ssh_utils import run_sshpass_command, copy_file_to_remote, PASSWORD
 
+def sudo(command: str):
+    return f" echo {PASSWORD} | sudo -S {command} "
+
 def main():
-    # copy_file_to_remote("web/cgi-bin/design_1_wrapper.bit.bin")
-    # run_sshpass_command(f"echo {PASSWORD} | sudo -S fpgautil -b design_1_wrapper.bit.bin")
+    copy_file_to_remote("web/cgi-bin/design_1_wrapper.bit.bin")
+    run_sshpass_command(sudo("fpgautil -b design_1_wrapper.bit.bin"))
+    run_sshpass_command(sudo("devmem 0x43c00000 w 480"))
+    run_sshpass_command(sudo("devmem 0x43c00008 w 2"))
 
     run_sshpass_command("rm -rf fiforeader && mkdir -p fiforeader")
     copy_file_to_remote("-r fiforeader", "~/")
-    # run_sshpass_command("cd fiforeader && gcc -g -o fiforeader fiforeader.c && ./fiforeader")
-    run_sshpass_command(f"cd fiforeader && gcc -g -o fiforeader *.c && echo {PASSWORD} | sudo -S ./fiforeader")
+    run_sshpass_command("cd fiforeader && gcc -g -o fiforeader *.c && " + sudo("./fiforeader 480000"))
 
 
 if __name__ == "__main__":
