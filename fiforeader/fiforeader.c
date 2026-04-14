0d21d3cf8d6f263b7135050b1b7ef41aa5aa7867
Jonathan Piland <jonathan.piland@duke.edu>
2026-04-13 22:40:52 -0400

    include radio control commands in fiforeader

--- fiforeader/fiforeader.c
+++ fiforeader/fiforeader.c
@@ -10,6 +10,21 @@ usage: ./fiforeader <number_of_words> \n\
   - Reads <number_of_words> 4-byte words from the AXI-Stream FIFO in the PL\
 "
 
+#define CONTROL_BASE_ADDR 0x43c00000
+#define CONTROL_BASE_SIZE 0x10
+
+#define CONTROL_DDS_RESET 0x1
+#define CONTROL_FIFO_ENABLE 0x2
+
+#define INITIAL_FAKEADC_PHASE_INCREMENT 480 // roughly Bb4
+
+typedef struct _radio_control_t {
+    uint32_t fakeadc_phase_increment;
+    uint32_t tuner_phase_increment;
+    uint32_t control_reg;
+    uint32_t readonly_clock;
+} radio_control_t;
+
 int main(int argc, char* argv[]) {
 
     if (argc != 2) {
@@ -32,30 +47,30 @@ int main(int argc, char* argv[]) {
                      fd,
                      XPAR_AXI_FIFO_MM_S_0_BASEADDR);
 
-    if (map == MAP_FAILED) {
+    void *control_map = mmap(NULL,
+                     CONTROL_BASE_SIZE,
+                     PROT_READ | PROT_WRITE,
+                     MAP_SHARED,
+                     fd,
+                     CONTROL_BASE_ADDR);
+
+    if (map == MAP_FAILED || control_map == MAP_FAILED) {
         perror("mmap");
         return -1;
     }
 
     volatile uint32_t *fifo_ptr = (volatile uint32_t *)map;
+    volatile radio_control_t* radio_control_ptr = (radio_control_t* ) control_map;
+
+    puts("Initializing radio...");
+    radio_control_ptr->fakeadc_phase_increment = INITIAL_FAKEADC_PHASE_INCREMENT;
+    radio_control_ptr->control_reg = CONTROL_FIFO_ENABLE; 
 
     puts("Initializing FIFO...");
     XLlFifo fifo;
     XLlFifo_Initialize(&fifo, (unsigned int) fifo_ptr);
     printf("Initialization complete! Reading %d 4-byte words...\n", num_words_to_read);
     fflush(stdout); // To ensure that timing can be reviewed accurately
-
-
-    // for (int i = 0; i < 17; i++) {
-    //     printf(" - fifo[BASE + 0x%02x] = 0x%08x\n", i * 4, fifo_ptr[i]);
-    // }
-
-    // for (int i = 0; i < 10; i++) {
-    //     int occ = XLlFifo_iRxOccupancy(&fifo);
-    //     printf("RX Occupancy: %d\n", occ);
-    // }
-
-    // unsigned int len = (XLlFifo_iRxGetLen(&fifo) & 0x3FFFF) >> 2;
 
     unsigned int num_words_read = 0;
     unsigned int single_sample;
--- test_fiforeader.py
+++ test_fiforeader.py
@@ -6,11 +6,10 @@ def sudo(command: str):
 def main():
     copy_file_to_remote("web/cgi-bin/design_1_wrapper.bit.bin")
     run_sshpass_command(sudo("fpgautil -b design_1_wrapper.bit.bin"))
-    run_sshpass_command(sudo("devmem 0x43c00000 w 480"))
-    run_sshpass_command(sudo("devmem 0x43c00008 w 2"))
 
     run_sshpass_command("rm -rf fiforeader && mkdir -p fiforeader")
     copy_file_to_remote("-r fiforeader", "~/")
+    print("Compiling `fiforeader` on the target... please wait...")
     run_sshpass_command("cd fiforeader && gcc -g -o fiforeader *.c && " + sudo("./fiforeader 480000"))
 
 
