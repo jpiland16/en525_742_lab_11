a3ca797bb1439f664d79c014488b7f75cef8b3fa
Jonathan Piland <jonathan.piland@duke.edu>
2026-04-14 21:23:46 -0400

    refactor folder structure

--- test_fiforeader.py
+++ test_fiforeader.py
@@ -1,3 +1,4 @@
+import glob
 from ssh_utils import run_sshpass_command, copy_file_to_remote, PASSWORD
 
 def sudo(command: str):
@@ -7,10 +8,11 @@ def main():
     copy_file_to_remote("web/cgi-bin/design_1_wrapper.bit.bin")
     run_sshpass_command(sudo("fpgautil -b design_1_wrapper.bit.bin"))
 
-    run_sshpass_command("rm -rf fiforeader && mkdir -p fiforeader")
-    copy_file_to_remote("-r fiforeader", "~/")
+    run_sshpass_command("rm -rf src && mkdir -p src")
+    copy_file_to_remote(" ".join(glob.glob("xilinxlib/*")), "~/src/.")
+    copy_file_to_remote("fiforeader/fiforeader.c", "~/src/.")
     print("Compiling `fiforeader` on the target... please wait...")
-    run_sshpass_command("cd fiforeader && gcc -g -o fiforeader *.c && " + sudo("./fiforeader 480000"))
+    run_sshpass_command("cd src && gcc -g -o ../fiforeader *.c && cd .. && " + sudo("./fiforeader 480000"))
 
 
 if __name__ == "__main__":
